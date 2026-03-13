3f10e3b7ad87fd17d07efe9c39a29a20d2b50644
partajoy <asbl.partajoy@gmail.com>
2026-03-13 15:19:44 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'voisin-service-v2.0';
+const urlsToCache = ['/', '/index.html', '/style.css', '/app.js'];
+
+self.addEventListener('install', event => {
+    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
